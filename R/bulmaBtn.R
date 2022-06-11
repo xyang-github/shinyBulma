916866bec670434b2da6c2c753a61ad8f014686f
Xing Yang <x.yang89@yahoo.com>
2022-06-11 14:40:40 -0700

    Update and fixed some bugs.

--- R/bulmaBtn.R
+++ R/bulmaBtn.R
@@ -34,7 +34,7 @@ BulmaBtn <- function(inputId,
                      inverted = FALSE,
                      rounded = FALSE,
                      disabled = FALSE,
-                     hidden = FALSE,
+                     hidden = FALSE
                      ) {
 
   size <- match.arg(
@@ -84,12 +84,12 @@ BulmaBtn <- function(inputId,
 }
 
 #' Update Bulma Button
-#'
 #' @param inputId string value for id
 #' @param text NULL or string value for button text
 #' @param color NULL or string value for button color
 #' @param size NULL or string value for button size
 #' @param state NULL or string value for button state
+#' @param spacing NULL or string value for margin and padding
 #' @param responsive NULL or Boolean value for responsive size
 #' @param fullwidth NULL or Boolean value to make button size fullwidth
 #' @param outlined NULL or Boolean value to make button outlined
@@ -97,7 +97,6 @@ BulmaBtn <- function(inputId,
 #' @param rounded NULL or Boolean value to make button with rounded corners
 #' @param disabled NULL or Boolean value to make a button disabled
 #' @param hidden NULL or Boolean value to make a button hidden
-#' @param spacing NULL or string value for margin and padding
 #' @param session session object
 #' @export
 UpdateBulmaBtn <- function(
@@ -106,6 +105,7 @@ UpdateBulmaBtn <- function(
     color = NULL,
     size = NULL,
     state = NULL,
+    spacing = NULL,
     responsive = NULL,
     fullwidth = NULL,
     outlined = NULL,
@@ -113,7 +113,6 @@ UpdateBulmaBtn <- function(
     rounded = NULL,
     disabled = NULL,
     hidden = NULL,
-    spacing = NULL,
     session = getDefaultReactiveDomain()
     ){
 
@@ -140,6 +139,14 @@ UpdateBulmaBtn <- function(
       )
     }
 
+    if (!is.null(spacing)) {
+      spacing <- match.arg(
+        arg = spacing,
+        choices = c("", "m-0", "m-1", "m-2", "m-3", "m-4", "m-5", "m-6","m-auto", "mt-0", "mt-1", "mt-2", "mt-3", "mt-4", "mt-5", "mt-6", "mt-auto", "mr-0",  "mr-1", "mr-2", "mr-3", "mr-4", "mr-5", "mr-6", "mr-auto", "mb-0", "mb-1", "mb-2", "mb-3", "mb-4", "mb-5", "mb-6","mb-auto",  "ml-0", "ml-1", "ml-2", "ml-3", "ml-4", "ml-5", "ml-6", "ml-auto", "mx-0",  "mx-1",  "mx-2",  "mx-3",  "mx-4",  "mx-5",  "mx-6",  "mx-auto",  "my-0", "my-1", "my-2", "my-3", "my-4", "my-5", "my-6", "my-auto", "p-0", "p-1", "p-2", "p-3", "p-4", "p-5", "p-6","p-auto", "pt-0", "pt-1", "pt-2", "pt-3", "pt-4", "pt-5", "pt-6", "pt-auto", "pr-0",  "pr-1", "pr-2", "pr-3", "pr-4", "pr-5", "pr-6", "pr-auto", "pb-0", "pb-1", "pb-2", "pb-3", "pb-4", "pb-5", "pb-6","pb-auto",  "pl-0", "pl-1", "pl-2", "pl-3", "pl-4", "pl-5", "pl-6", "pl-auto", "px-0",  "px-1",  "px-2",  "px-3",  "px-4",  "px-5",  "px-6",  "px-auto",  "py-0", "py-1", "py-2", "py-3", "py-4", "py-5", "py-6", "py-auto"),
+        several.ok = TRUE
+      )
+    }
+
     # Makes sure that the attribute contains either TRUE or FALSE if not NULL
     Is_Null <- function(attribute) {
       if (!is.null(attribute) & !is.logical(attribute)) {
@@ -161,14 +168,14 @@ UpdateBulmaBtn <- function(
       color = color,
       size = size,
       state = state,
+      spacing = spacing,
       responsive = responsive,
       fullwidth = fullwidth,
       outlined = outlined,
       inverted = inverted,
       rounded = rounded,
       disabled = disabled,
-      hidden = hidden,
-      spacing = spacing
+      hidden = hidden
       )
 
     # Removes elements that have a NULL value
