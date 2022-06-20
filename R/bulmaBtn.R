df9c7873db866cc8a34e60e97ae6cabe49d41772
Xing Yang <x.yang89@yahoo.com>
2022-06-20 09:09:44 -0700

    Added code for including fontawesome icons to buttons

--- R/bulmaBtn.R
+++ R/bulmaBtn.R
@@ -24,6 +24,7 @@
 #' BulmaBtn(inputId = "example", text = "Submit")
 BulmaBtn <- function(inputId,
                      text,
+                     icon,
                      size = "is-normal",
                      color = NULL,
                      state = NULL,
@@ -37,11 +38,13 @@ BulmaBtn <- function(inputId,
                      hidden = FALSE
                      ) {
 
+  # Size argument matching
   size <- match.arg(
     arg = size,
     choices = c("is-normal", "is-small", "is-medium", "is-large")
   )
 
+  # Color argument matching
   color <- match.arg(
     arg = color,
     choices = c("", "is-white", "is-light", "is-dark", "is-black", "is-text",
@@ -49,18 +52,44 @@ BulmaBtn <- function(inputId,
                 "is-warning", "is-danger")
   )
 
+  # State argument matching
   state <- match.arg(
     arg = state,
     choices = c("", "is-hovered", "is-focused", "is-active", "is-loading")
   )
 
+  # Spacing argument matching
   spacing <- match.arg(
     arg = spacing,
     choices = c("", "m-0", "m-1", "m-2", "m-3", "m-4", "m-5", "m-6","m-auto", "mt-0", "mt-1", "mt-2", "mt-3", "mt-4", "mt-5", "mt-6", "mt-auto", "mr-0",  "mr-1", "mr-2", "mr-3", "mr-4", "mr-5", "mr-6", "mr-auto", "mb-0", "mb-1", "mb-2", "mb-3", "mb-4", "mb-5", "mb-6","mb-auto",  "ml-0", "ml-1", "ml-2", "ml-3", "ml-4", "ml-5", "ml-6", "ml-auto", "mx-0",  "mx-1",  "mx-2",  "mx-3",  "mx-4",  "mx-5",  "mx-6",  "mx-auto",  "my-0", "my-1", "my-2", "my-3", "my-4", "my-5", "my-6", "my-auto", "p-0", "p-1", "p-2", "p-3", "p-4", "p-5", "p-6","p-auto", "pt-0", "pt-1", "pt-2", "pt-3", "pt-4", "pt-5", "pt-6", "pt-auto", "pr-0",  "pr-1", "pr-2", "pr-3", "pr-4", "pr-5", "pr-6", "pr-auto", "pb-0", "pb-1", "pb-2", "pb-3", "pb-4", "pb-5", "pb-6","pb-auto",  "pl-0", "pl-1", "pl-2", "pl-3", "pl-4", "pl-5", "pl-6", "pl-auto", "px-0",  "px-1",  "px-2",  "px-3",  "px-4",  "px-5",  "px-6",  "px-auto",  "py-0", "py-1", "py-2", "py-3", "py-4", "py-5", "py-6", "py-auto"),
     several.ok = TRUE
   )
 
-  html <- tags$button(
+  # Create HTML object based on icon
+  if (!is.null(icon)) {
+    html <- tags$button(
+              id = inputId,
+              class = "button bulma-btn",
+              class = size,
+              class = color,
+              class = state,
+              class = spacing,
+              class = if (responsive) "is-responsive",
+              class = if (fullwidth) "is-fullwidth",
+              class = if (outlined) "is-outlined",
+              class = if (inverted) "is-inverted",
+              class = if (rounded) "is-rounded",
+              class = if (hidden) "is-hidden",
+              tags$span(
+                class = "icon",
+                HTML("<i class = '", icon, "'></i>")
+                ),
+              tags$span(
+                text
+                )
+              )
+    } else {
+        html <- tags$button(
             id = inputId,
             class = "button bulma-btn",
             class = size,
@@ -74,11 +103,13 @@ BulmaBtn <- function(inputId,
             class = if (rounded) "is-rounded",
             class = if (hidden) "is-hidden",
             text)
+    }
 
   if (disabled) html <- tagQuery(html)$addAttrs("disabled" = "true")$selected()
 
   tagList(html,
           Bulma_Dependency(),
+          Font_Awesome_Dependency(),
           Bulma_Button_Binding()
           )
 }
