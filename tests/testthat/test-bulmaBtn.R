5fe3d4cd099279269851087b6d968dd412917aa7
Xing Yang <x.yang89@yahoo.com>
2022-06-22 17:18:00 -0700

    Changing tests based on changes made on source code

--- tests/testthat/test-bulmaBtn.R
+++ tests/testthat/test-bulmaBtn.R
@@ -1,4 +1,4 @@
-test_that("BulmaBtn renders correct html tags", {
+test_that("BulmaBtn renders correct html tags without icons", {
   tag <- tagQuery(
     BulmaBtn(
       inputId = "example",
@@ -17,34 +17,147 @@ test_that("BulmaBtn renders correct html tags", {
                                rounded = TRUE,
                                hidden = TRUE))
 
-  expected <- paste("<button class=\"button bulma-btn is-large is-link",
-                    "is-loading  is-responsive is-fullwidth is-outlined",
-                    "is-inverted is-rounded is-hidden\"",
-                    "id=\"example\">Button</button>")
+  expected <- paste(
+    "<button class=\"button bulma-btn is-large is-link is-loading",
+    "is-responsive is-fullwidth is-outlined is-inverted is-rounded",
+    "is-hidden\" id=\"example\">Button</button>")
 
   expect_type(tag, "list")
-  expect_equal(str,expected)
+  expect_equal(str, expected)
+})
+
+test_that("BulmaBtn renders correct html tags with icons", {
+  tag <- tagQuery(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button")
+    )
+
+  str <- as.character(BulmaBtn(
+    inputId = "example",
+    icon = "fa-solid fa-check",
+    text = "Button",
+    color = "is-link",
+    size = "is-large",
+    state = "is-loading",
+    responsive = TRUE,
+    fullwidth = TRUE,
+    outlined = TRUE,
+    inverted = TRUE,
+    rounded = TRUE,
+    hidden = TRUE))
+
+  expected <- paste(
+    "<button class=\"button bulma-btn is-large is-link is-loading",
+    "is-responsive is-fullwidth is-outlined is-inverted is-rounded",
+    "is-hidden\" id=\"example\">\n  <span class=\"icon\"><i class",
+    "= ' fa-solid fa-check '></i></span>\n  <span>Button</span>\n</button>"
+  )
+
+  expect_type(tag, "list")
+  expect_equal(str, expected)
 })
 
 test_that("That incorrect argument choices will cause an error", {
 
-  result <- paste("<button class=\"button bulma-btn is-normal   \"",
-                  "id=\"example\">Button</button>")
+  # No arguments
+  expect_error(BulmaBtn())
 
-  expect_error(BulmaBtn())  # no arguments
-  expect_error(BulmaBtn(inputId = "example", color = "is-light"))  # no text argument
-  expect_error(as.character(BulmaBtn(inputId = "example",
-                                     text = "Button",
-                                     color = "red")))  # wrong color choice
-  expect_error(as.character(BulmaBtn(inputId = "example",
-                                     text = "Button",
-                                     size = "xl")))  # wrong size choice
-  expect_error(as.character(BulmaBtn(inputId = "example",
-                                     text = "Button",
-                                     state = "disabled")))  # wrong state choice
-  expect_equal(as.character(BulmaBtn(inputId = "example",
-                                     text = "Button")),result)
+  # No text argument
+  expect_error(BulmaBtn(inputId = "example", color = "is-light"))
 
+  # Wrong color choice
+  expect_error(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      color = "red"))
+
+  # Blank color choice
+  expect_error(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      color = ""))
+
+  # Wrong size choice
+  expect_error(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      size = "xl"))
+
+  # Wrong state choice
+  expect_error(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      state = "disabled"))
+
+  # Wrong space choice
+  expect_error(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      spacing = "padding-0"))
+
+  # Wrong responsiveness choice
+  expect_error(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      responsive = "yes"))
+
+  # Wrong fullwidth choice
+  expect_error(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      fullwidth = "yes"))
+
+  # Wrong outlined choice
+  expect_error(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      outlined = "yes"))
+
+  # Wrong inverted choice
+  expect_error(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      inverted = ""))
+
+  # Wrong rounded choice
+  expect_error(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      rounded = "yes"))
+  # Wrong disabled choice
+  expect_error(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      disabled = "yes"))
+
+  # Wrong hidden choice
+  expect_error(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      hidden = "yes"))
+})
+
+test_that("Wrong icon argument will run silently", {
+
+  expect_silent(
+    BulmaBtn(
+      inputId = "example",
+      text = "Button",
+      icon = "blah")
+  )
 })
 
 test_that("Disable attribute works", {
