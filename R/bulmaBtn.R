b3435ef7ce0899b5c94a0805696e2f1bdff73240
Xing Yang <x.yang89@yahoo.com>
2022-06-24 16:27:30 -0700

    Format code to stay within 80 character width

--- R/bulmaBtn.R
+++ R/bulmaBtn.R
@@ -17,12 +17,25 @@
 #' @param rounded if true, will create a rounded button
 #' @param disabled if true, will disable the button
 #' @param hidden if true, will hide the button from view
-#' @param spacing string value for padding and margin; choices are: "m-0", "m-1", "m-2", "m-3", "m-4", "m-5", "m-6","m-auto", "mt-0", "mt-1", "mt-2", "mt-3", "mt-4", "mt-5", "mt-6", "mt-auto", "mr-0",  "mr-1", "mr-2", "mr-3", "mr-4", "mr-5", "mr-6", "mr-auto", "mb-0", "mb-1", "mb-2", "mb-3", "mb-4", "mb-5", "mb-6","mb-auto",  "ml-0", "ml-1", "ml-2", "ml-3", "ml-4", "ml-5", "ml-6", "ml-auto", "mx-0",  "mx-1",  "mx-2",  "mx-3",  "mx-4",  "mx-5",  "mx-6",  "mx-auto",  "my-0", "my-1", "my-2", "my-3", "my-4", "my-5", "my-6", "my-auto", "p-0", "p-1", "p-2", "p-3", "p-4", "p-5", "p-6","p-auto", "pt-0", "pt-1", "pt-2", "pt-3", "pt-4", "pt-5", "pt-6", "pt-auto", "pr-0",  "pr-1", "pr-2", "pr-3", "pr-4", "pr-5", "pr-6", "pr-auto", "pb-0", "pb-1", "pb-2", "pb-3", "pb-4", "pb-5", "pb-6","pb-auto",  "pl-0", "pl-1", "pl-2", "pl-3", "pl-4", "pl-5", "pl-6", "pl-auto", "px-0",  "px-1",  "px-2",  "px-3",  "px-4",  "px-5",  "px-6",  "px-auto",  "py-0", "py-1", "py-2", "py-3", "py-4", "py-5", "py-6", "py-auto",
+#' @param spacing string value for padding and margin; choices are:
+#' "m-0", "m-1", "m-2", "m-3", "m-4", "m-5", "m-6","m-auto", "mt-0", "mt-1",
+#' "mt-2", "mt-3", "mt-4", "mt-5", "mt-6", "mt-auto", "mr-0",  "mr-1", "mr-2",
+#' "mr-3", "mr-4", "mr-5", "mr-6", "mr-auto", "mb-0", "mb-1", "mb-2", "mb-3",
+#' "mb-4", "mb-5", "mb-6","mb-auto",  "ml-0", "ml-1", "ml-2", "ml-3", "ml-4",
+#' "ml-5", "ml-6", "ml-auto", "mx-0",  "mx-1",  "mx-2",  "mx-3",  "mx-4",
+#' "mx-5",  "mx-6",  "mx-auto",  "my-0", "my-1", "my-2", "my-3", "my-4",
+#' "my-5", "my-6", "my-auto", "p-0", "p-1", "p-2", "p-3", "p-4", "p-5",
+#' "p-6","p-auto", "pt-0", "pt-1", "pt-2", "pt-3", "pt-4", "pt-5", "pt-6",
+#' "pt-auto", "pr-0",  "pr-1", "pr-2", "pr-3", "pr-4", "pr-5", "pr-6",
+#' "pr-auto", "pb-0", "pb-1", "pb-2", "pb-3", "pb-4", "pb-5", "pb-6","pb-auto",
+#' "pl-0", "pl-1", "pl-2", "pl-3", "pl-4", "pl-5", "pl-6", "pl-auto", "px-0",
+#' "px-1",  "px-2",  "px-3",  "px-4",  "px-5",  "px-6",  "px-auto",  "py-0",
+#' "py-1", "py-2", "py-3", "py-4", "py-5", "py-6", "py-auto",
 #' @import htmltools
 #' @export
 #' @examples
 #' BulmaBtn(inputId = "example", text = "Submit")
-BulmaBtn <- function(inputId,
+BulmaBtn <- function(inputId, # nolint
                      text,
                      size = "is-normal",
                      icon = NULL,
@@ -66,7 +79,20 @@ BulmaBtn <- function(inputId,
   if (!is.null(spacing)) {
     spacing <- match.arg(
     arg = spacing,
-    choices = c("m-0", "m-1", "m-2", "m-3", "m-4", "m-5", "m-6","m-auto", "mt-0", "mt-1", "mt-2", "mt-3", "mt-4", "mt-5", "mt-6", "mt-auto", "mr-0",  "mr-1", "mr-2", "mr-3", "mr-4", "mr-5", "mr-6", "mr-auto", "mb-0", "mb-1", "mb-2", "mb-3", "mb-4", "mb-5", "mb-6","mb-auto",  "ml-0", "ml-1", "ml-2", "ml-3", "ml-4", "ml-5", "ml-6", "ml-auto", "mx-0",  "mx-1",  "mx-2",  "mx-3",  "mx-4",  "mx-5",  "mx-6",  "mx-auto",  "my-0", "my-1", "my-2", "my-3", "my-4", "my-5", "my-6", "my-auto", "p-0", "p-1", "p-2", "p-3", "p-4", "p-5", "p-6","p-auto", "pt-0", "pt-1", "pt-2", "pt-3", "pt-4", "pt-5", "pt-6", "pt-auto", "pr-0",  "pr-1", "pr-2", "pr-3", "pr-4", "pr-5", "pr-6", "pr-auto", "pb-0", "pb-1", "pb-2", "pb-3", "pb-4", "pb-5", "pb-6","pb-auto",  "pl-0", "pl-1", "pl-2", "pl-3", "pl-4", "pl-5", "pl-6", "pl-auto", "px-0",  "px-1",  "px-2",  "px-3",  "px-4",  "px-5",  "px-6",  "px-auto",  "py-0", "py-1", "py-2", "py-3", "py-4", "py-5", "py-6", "py-auto"),
+    choices = c("m-0", "m-1", "m-2", "m-3", "m-4", "m-5", "m-6","m-auto",
+    "mt-0", "mt-1", "mt-2", "mt-3", "mt-4", "mt-5", "mt-6", "mt-auto",
+    "mr-0",  "mr-1", "mr-2", "mr-3", "mr-4", "mr-5", "mr-6", "mr-auto", "mb-0",
+    "mb-1", "mb-2", "mb-3", "mb-4", "mb-5", "mb-6","mb-auto",  "ml-0", "ml-1",
+    "ml-2", "ml-3", "ml-4", "ml-5", "ml-6", "ml-auto", "mx-0",  "mx-1",
+    "mx-2",  "mx-3",  "mx-4",  "mx-5",  "mx-6",  "mx-auto",  "my-0", "my-1",
+    "my-2", "my-3", "my-4", "my-5", "my-6", "my-auto", "p-0", "p-1", "p-2",
+    "p-3", "p-4", "p-5", "p-6", "p-auto", "pt-0", "pt-1", "pt-2", "pt-3",
+    "pt-4", "pt-5", "pt-6", "pt-auto", "pr-0",  "pr-1", "pr-2", "pr-3",
+    "pr-4", "pr-5", "pr-6", "pr-auto", "pb-0", "pb-1", "pb-2", "pb-3",
+    "pb-4", "pb-5", "pb-6", "pb-auto",  "pl-0", "pl-1", "pl-2", "pl-3",
+    "pl-4", "pl-5", "pl-6", "pl-auto", "px-0",  "px-1",  "px-2", "px-3",
+    "px-4",  "px-5",  "px-6",  "px-auto",  "py-0", "py-1", "py-2", "py-3",
+    "py-4", "py-5", "py-6", "py-auto"),
     several.ok = TRUE
     )
   }
@@ -186,7 +212,7 @@ UpdateBulmaBtn <- function(
 
     # Makes sure that the attribute contains either TRUE or FALSE if not NULL
     Is_Null <- function(attribute) {
-      if (!is.null(attribute) & !is.logical(attribute)) {
+      if (!is.null(attribute) && !is.logical(attribute)) {
         stop("One of the parameters in UpdateBulmaBtn must be TRUE or FALSE")
       }
     }
@@ -198,7 +224,6 @@ UpdateBulmaBtn <- function(
     Is_Null(rounded)
     Is_Null(disabled)
     Is_Null(hidden)
-    Is_Null(spacing)
 
     message <- list(
       text = text,
