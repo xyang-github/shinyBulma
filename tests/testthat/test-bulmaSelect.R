fe93be918e5031927cc365c23f4b37a7fa6c79b3
Xing Yang <x.yang89@yahoo.com>
2022-07-01 11:23:39 +0000

    Added more test to BulmaSelect

--- tests/testthat/test-bulmaSelect.R
+++ tests/testthat/test-bulmaSelect.R
@@ -35,3 +35,35 @@ test_that("Select widget with no additional class attributes renders html
 
   expect_equal(as.character(select), answer)
 })
+
+test_that("Class attributes are formatting correctly", {
+  select <- BulmaSelect(
+    inputId = "example",
+    size = "is-large",
+    choices = c("A", "B", "C"),
+    color = "is-white",
+    state = "is-active",
+    spacing = c("m-5", "p-5"),
+    rounded = TRUE,
+    responsive = TRUE,
+    fullwidth = TRUE,
+    hidden = TRUE)
+
+  answer <- paste0("<select class=\"is-white is-large is-active m-5 p-5 is-res",
+                   "ponsive is-fullwidth is-rounded is-hidden\" id=\"examp",
+                   "le\">\n  <option>A</option>\n  <option>B</option>\n  <op",
+                   "tion>C</option>\n</select>")
+
+  expect_true(as.character(select), answer)
+})
+
+test_that("Icon attribute works", {
+  select <- BulmaSelect(
+    inputId = "example",
+    choices = c("A", "B", "C"),
+    icon = "fa-solid fa-globe"
+  )
+
+  print(select)
+  print(as.character(select))
+})
